d24db3e1a6140c694f17cf56bc6008f9921c224d
xaxaxa01 <krispenerators@mail.ru>
2023-11-21 10:05:04 +0300

    последовательность

---
+++ задание с последовательностью+/задание с последовательностью/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+string str = "";
+int lenght;
+bool result;
+int number;
+do
+{
+    Console.WriteLine("введите челое положительное число: ");
+    str = Convert.ToString(Console.ReadLine());
+    lenght = str.Length;
+    result = int.TryParse(str, out int num);
+    number = Convert.ToInt32(str);
+} while(lenght == 0 || result == false || number == 0);
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(i + " ");
+}
+for (int i = number - 1; i >= 1; i--)
+{
+    Console.Write(i + " ");
+}
