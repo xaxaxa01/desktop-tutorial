d6f4bcc710d719522f8e2d3f11669f3ee6c55cce
xaxaxa01 <krispenerators@mail.ru>
2023-11-21 10:03:04 +0300

    лаб3

---
+++ лабораторная3+/лабораторная3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace лабораторная3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ListBox notesList = new ListBox();
+            //notesList.Items.Add("test 1");
+            //notesList.Items.Add("test 2");
+            //notesList.Items.Add("test 3");
+            //test.Items.Add(new TabItem
+            //{
+            //    Header = new TextBlock { Text = "третья вкладка" },
+            //    Content = notesList
+            //}
+            //);
+        }
+
+        //private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        //{
+        //    TreeViewItem tvItem = (TreeViewItem)sender;
+        //    MessageBox.Show("Узел " + tvItem.Header.ToString() + " раскрыт");
+        //}
+        //private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        //{
+        //    TreeViewItem tvItem = (TreeViewItem)sender;
+        //    MessageBox.Show("Выбран узел: " + tvItem.Header.ToString());
+        //}
+
+
+
+    }
+}
